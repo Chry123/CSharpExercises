f15bd89ac85c4c3988dc6017766e1122a5923e46
Chry123 <christianshoraj@gmail.com>
2021-01-07 15:31:13 +0100

    This is the #3 exercise

--- Ecomerce/EComerce.cs
+++ Ecomerce/EComerce.cs
@@ -1,80 +1,10 @@
-//PROGRAM.CS ES
-/*using ECommerce;
-
-namespace CSharpExercises
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            for (int i = 0; i < args.Length; i++)
-            {
-                Console.WriteLine(args[i].ToUpper());
-            }
-
-            // var firstCustomer = new Customer();
-            Costomer firstCostomer = new Costomer("Romeo", "Colosseo", "romeocoloss@gmail.com");
-            firstCostomer.Login();
-
-            Costomer secondCostomer = new Costomer("Mario", "Rossi", "m.rossi99@hotmail.com");
-            secondCostomer.Login();
-
-
-            //class Article
-
-            Article article = new Article("Face mask", 15.54);
-            //Article article2 = new Article  (" Sometime ", 50.99, );
-
-            article.List();
-            article.Retrieve(0);
-            article.Destroy(0);
-
-            Console.WriteLine(article.Description);
-
-            article.Description = "articolo"; 
-
-
-            Console.WriteLine($"{article.Description} price is : {article.Price}");
-
-            
-
-            OrderHeader orderHeader = new OrderHeader(20, DateTime.Now);
-
-            orderHeader.List();
-            orderHeader.Retrieve(0);
-            orderHeader.Destroy(0);
-            
-            Console.WriteLine($"Date: {orderHeader.Date}, UserID: {orderHeader.UserId}");
-
-
-            Management management = new Management()
-
-
-            //article2.Order(secondCustomer);  
-
-
-            
-        }
-    }
-}
-*/
-
-
 using System;
 
 namespace ECommerce
 {
-    class Costomer
+    class Customer
     {
-        // Fields or Attributes
-        // Più privati possibili
-
-        // Properties
-        // getter - setter
-
-        // private string _X
-        // _X -> Underscore convenzione che indica attributo privato
-
+        
         protected int Id;
         protected string FirstName;
         protected string LastName;
@@ -84,8 +14,10 @@ namespace ECommerce
         protected string Password;
         public int Age { get; set;}
 
+
+
         // Constructor
-        public Costomer(string firstName, string lastName, string email)
+        public Customer(string firstName, string lastName, string email)
         {
             // Called on instantiation
             this.FirstName = firstName;
@@ -93,32 +25,18 @@ namespace ECommerce
             this.Email = email;
         }
     
-        // Methods
+
         // visibility - returned value - MethodName()
         public void Login()
         {
-            Console.WriteLine($"Hi {this.FirstName} {this.LastName}, you are logged in.");
+            Console.WriteLine($"Ciao {this.FirstName} {this.LastName}, you are logged in.");
         }
-        public void CheckOut()
+        public  static void List() 
         {
-            Console.WriteLine("Product(s) purchased.");
+            Console.WriteLine("");
         }
-        public void MyOrders()
-        {
-            Console.WriteLine("These are your orders.");
-        }
-        public void WishList()
-        {
-            Console.WriteLine("This is your wishlist.");
-        }
-        public void AddToCart()
-        {
-                Console.WriteLine("article added to cart!");
-        }
-        public void Signin()
-        {
-            Console.WriteLine("You are now signed in.");
-        }
-        }
+        
+        
+    }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,43 +1,34 @@
-using System;
-using ExLinq;
+﻿using System;
+using System.Collections.Generic;
+using ECommerce;
 
-namespace ESExLinq2
+namespace Customers
 {
-        /*class ExLinq2
+    class Customers 
+    {
+        public  static void  Main(string[] args)
         {
-            public static void Main(string[] args)
-            {
-                var cities = ExLinq.Filtering("Firenze");
 
-                foreach (var city in cities)
-                {
-                    Console.WriteLine("E' una citta");
-                }
-            }
-
-
-            class Mountains
-            {
-                public static void Main(string[] args)
-                {
-                var result = ESExLinq.Mountains.ListOfMountains(""); 
-
-                     Console.WriteLine(result);
-
-                var NameMontain = "Monte Falco, 1658, Parco Foreste Casentinesi ; Monte Falterona, 1654, Parco Foreste Casentinesi; Monte Fumaiolo, 1407, Appennino Tosco Emiliano";
-                
+      
+                Customer firstCostomer = new Customer("Romeo", "Colosseo", "romeocoloss@gmail.com");
+                firstCostomer.Login();
+                Customer secondCustomer = new Customer("Mario", "Rossi", "m.rossi99@hotmail.com");
+                secondCustomer.Login();
                 
 
-                    foreach (var element  in result )
+                var list = new List<string> { "Luigi", "Mario", "Felipe" };
+
+                    foreach (var name in list)
                     {
-                        Console.WriteLine(NameMontain.Split(',')[0].Trim()); 
-                    } 
+                    Console.WriteLine($"{name}");
+                    }
 
+                //Customer.GetListOfArticles();
+        }
 
-                }
-
-            }
-
-}
+    }
+                   
+            
+} 
 
 
